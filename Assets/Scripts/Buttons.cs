8a323f94b5558862323ab66b4a7847ab4aabc4ad
Alessia Frey <lateacherdemate@gmail.com>
2025-09-19 14:06:57 -0300

    Button
    Button

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject upDoor;
+    public GameObject downDoor;
+    private Animator upDoorAnimator;
+    private Animator downDoorAnimator;
+    private Animator animator;
+
+    private bool activated = false;
+
     void Start()
     {
-        
+        upDoorAnimator = upDoor.GetComponent<Animator>();
+        downDoorAnimator = downDoor.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (activated) return;
+            upDoorAnimator.SetTrigger("Open");
+            downDoorAnimator.SetTrigger("Open");
+            upDoor.GetComponent<Collider2D>().enabled = false;
+            downDoor.GetComponent<Collider2D>().enabled = false;
+            animator.SetTrigger("Activated");
+
+        }
     }
+
 }
